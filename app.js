a19e77f4515083db9676ceddf3e262a5fd029850
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2017-09-27 11:50:39 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 console.log('Starting app!');
 
 const fs = require('fs');
+const os = require('os');
 
-fs.appendFile('greetings.txt','Hello World!');
+var user = os.userInfo();
+
+fs.appendFile('greetings.txt','Hello '+ user.username + '!');
