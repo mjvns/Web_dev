c283fdec745e4476e51c9a8cb6f6d9b907246781
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2017-09-28 12:34:32 +0530

    Update app.js

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,16 +1,7 @@
 console.log('Starting app.js!');
 
 const fs = require('fs');
-const os = require('os');
 const notes = require('./notes.js');
 
 var res = notes.addNote();
 console.log(res);
-
-var sum = notes.add(-8,1);
-console.log(sum);
-
-
-//var user = os.userInfo();
-
-//fs.appendFile('greetings.txt',`Hello ${user.username}! You are ${notes.age}`);
