a9c919daa376306ff93fae4de0ace505970f3d7a
Madhukar Jaiswal <madhukar0206jaiswal@gmail.com>
2017-09-28 15:48:31 +0530

    Add files via upload
    Starts to introduce the functionalities for various note taking operations.

--- NotesApp/app.js
+++ NotesApp/app.js
@@ -1,7 +1,21 @@
 console.log('Starting app.js!');
 
 const fs = require('fs');
+const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-var res = notes.addNote();
-console.log(res);
+var command = process.argv[2];
+console.log('Command:',command);
+
+if(command === 'add'){
+	console.log('adding new list!');
+}else if(command === 'list'){
+	console.log('Listing items in the note!');
+}else if(command === 'read'){
+	console.log('Fetching the node!');
+}else if(command === 'remove'){
+	console.log('removing the node!');
+}else{
+	console.log('command not recognised!');
+}
